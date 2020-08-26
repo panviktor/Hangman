15a8fc8b7a2a02d46a58d8935ec4a18fd5e1d02c
SwiftCoder <SwiftCoder@nossvwpdf.ls>
2020-08-26 17:06:30 +0300

    Initial Commit

---
+++ Hangman/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hangman
+//
+//  Created by Viktor on 26.08.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
